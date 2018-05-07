0556c63a259df5fc5db255bdfcce0a29f84ccee3
Mikko Koppanen <mikko.koppanen@alltimetech.co.uk>
2018-05-07 14:48:40 +0100

    Add test for unwrapping with view

--- src/test/java/com/fasterxml/jackson/module/jsonSchema/TestUnwrapping.java
+++ src/test/java/com/fasterxml/jackson/module/jsonSchema/TestUnwrapping.java
@@ -1,10 +1,27 @@
 package com.fasterxml.jackson.module.jsonSchema;
 
 import com.fasterxml.jackson.annotation.JsonUnwrapped;
+import com.fasterxml.jackson.annotation.JsonView;
+import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class TestUnwrapping extends SchemaTestBase
 {
+    interface InViewMarker {}
+
+    static class IdType {
+        public int id;
+    }
+
+    static class UnwrapWithView {
+
+        @JsonView({ InViewMarker.class })
+        @JsonUnwrapped
+        public IdType id;
+    }
+
+
+
     static class UnwrappingRoot
     {
         public int age;
@@ -25,6 +42,10 @@ public class TestUnwrapping extends SchemaTestBase
     
     private final ObjectMapper MAPPER = objectMapper();
 
+    private final String EXP = "{'type':'object'," +
+        "'id':'urn:jsonschema:com:fasterxml:jackson:module:jsonSchema:TestUnwrapping:UnwrappingRoot'," +
+        "'properties':{'age':{'type':'integer'},'name.first':{'type':'string'},'name.last':{'type':'string'}}}";
+
     public void testUnwrapping()  throws Exception
     {
         JsonSchemaGenerator generator = new JsonSchemaGenerator(MAPPER);
@@ -33,9 +54,28 @@ public class TestUnwrapping extends SchemaTestBase
         String json = MAPPER.writeValueAsString(schema).replace('"', '\'');
         
 //System.err.println("JSON -> "+MAPPER.writerWithDefaultPrettyPrinter().writeValueAsString(schema));
-        String EXP = "{'type':'object'," +
-                     "'id':'urn:jsonschema:com:fasterxml:jackson:module:jsonSchema:TestUnwrapping:UnwrappingRoot'," +
-                     "'properties':{'age':{'type':'integer'},'name.first':{'type':'string'},'name.last':{'type':'string'}}}";
         assertEquals(EXP, json);
+    }
+
+    public void testUnwrappingWithJsonView()  throws Exception
+    {
+        ObjectMapper mm = new ObjectMapper();
+        mm.disable(MapperFeature.DEFAULT_VIEW_INCLUSION);
+        mm.setConfig(
+                mm.getSerializationConfig().withView(InViewMarker.class)
+        );
+
+        JsonSchemaGenerator withViewGenerator = new JsonSchemaGenerator(mm);
+        JsonSchema schemaWithView = withViewGenerator.generateSchema(UnwrapWithView.class);
+
+        String jsonWithView = MAPPER.writeValueAsString(schemaWithView).replace('"', '\'');
+
+        JsonSchemaGenerator generator = new JsonSchemaGenerator(MAPPER);
+        JsonSchema schema = generator.generateSchema(UnwrapWithView.class);
+
+        String json = MAPPER.writeValueAsString(schema).replace('"', '\'');
+
+//System.err.println("JSON -> "+MAPPER.writerWithDefaultPrettyPrinter().writeValueAsString(schema));
+        assertEquals(json, jsonWithView);
     }
 }
